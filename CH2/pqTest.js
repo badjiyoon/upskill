a17a204a34f41ee5aaf877cfc19606953899e780
badjiyoon <badjiyoon1@gmail.com>
2024-06-04 23:56:37 +0900

    김지윤 커밋 처리

---
+++ CH2/pqTest.js
@@ -0,0 +1,13 @@
+const PriorityQueue = require('priorityqueuejs');
+
+const queue = new PriorityQueue((a, b) => {
+	return a.cash - b.cash;
+});
+
+queue.enq({ cash: 250, name: 'Valentina' });
+queue.enq({ cash: 300, name: 'Jano' });
+queue.enq({ cash: 150, name: 'Fran' });
+console.log(queue.size()); // 3
+console.log(queue.deq()); // { cash: 300, name: 'Jano' }
+console.log(queue.peek()); // { cash: 250, name: 'Valentina' }
+console.log(queue.size()); // 2
