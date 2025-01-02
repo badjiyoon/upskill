0719bd2968928a744b9024f8b7b43b7ede73caa6
badjiyoon <badjiyoon1@gmail.com>
2025-01-02 22:32:16 +0900

    김지윤 / 작업 추가

--- CH4/BJ_1946.js
+++ CH4/BJ_1946.js
@@ -8,18 +8,18 @@ for (let i = 0; i < tcCnt; i++) {
 	const n = Number(input[line]);
 	let arr = [];
 	for (let j = line + 1; j <= line + n; j++) {
-		const [a,b ] = input[j].split(' ').map(Number);
-		arr.push([a, b]);
+		const data = input[j].split(' ').map(Number);
+		arr.push(data);
 	}
 
 	arr.sort((x, y) => x[0] - y[0]);
 	let count = 0;
-	let minVal = 10001;
+	let minVal = 100001;
 
 	for (let [x, y] of arr) {
 		if (y < minVal) {
 			minVal = y;
-			count++;
+			count += 1;
 		}
 	}
 	console.log(count);
